58c4b243a01a5155488273bf744bcbbb07f8f432
RenanGAS <renansakaoki@gmail.com>
2022-01-07 14:31:10 -0300

    feat: create a main() function to execute the method, and f() function to substitute the values in the expression

--- Bisection Method/Bisection Method.py
+++ Bisection Method/Bisection Method.py
@@ -1,5 +1,32 @@
 from sympy import symbols
 
-x, y = symbols('x y')
-expr = x + 2 * y
-print(expr)
+print("\nMétodo da Bissecante")
+
+print("\nDigite a função:")
+str_f = input("\nf(x) = ")
+
+def f(x):
+    func = eval(str_f)
+    return func
+
+def main():
+    print("\nEscolha um intervalo [a, b] tal que f(a) . f(b) < 0 :")
+    a = float(input("\na = "))
+    b = float(input("\nb = "))
+
+    if f(a)*f(b) > 0:
+        print("\nErro: f(",a,") =", f(a), ", f(",b,") =",f(b))
+        a = float(input("\na = "))
+        b = float(input("\nb = "))
+
+    print("\nEntradas aceitas: f(",a,") =", f(a),", f(",b,") =",f(b))
+    
+    if a > b:
+        aux = b
+        b = a
+        a = aux
+
+    print("\nIntervalo definido: [",a,",",b,"]")
+
+if __name__ == "__main__":
+    main()

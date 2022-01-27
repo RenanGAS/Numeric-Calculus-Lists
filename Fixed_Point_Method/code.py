0db88003db92ea5732604f35b1d03c4fb9b3f6c5
RenanGAS <renansakaoki@gmail.com>
2022-01-27 17:49:51 -0300

    feat: finish the Fixed Point Method's algorithm

--- Fixed_Point_Method/code.py
+++ Fixed_Point_Method/code.py
@@ -1,32 +1,76 @@
 from sympy import *
-import math
 from prettytable import PrettyTable
 
-def fixedPointMethod(g, xk, stopC, maxIt):
+def fixedPointMethod(F, xK, stopC):
 
     t = PrettyTable()                                                   # Cria a tabela
-    t.field_names = ["K", "xK-1 / x", "xK", "xK+1", "|xK+1 - xK|"]      # Define as colunas
-
+    t.field_names = ["K", "xK", "xK+1", "|xK+1 - xK|"]                  # Define as colunas
+    
+    numIt = 0
+    xKPlus1 = 0
+    
+    while numIt < 200 :
+        
+        xKPlus1 = F(xK)                                                 # Executa o Método
+        
+        t.add_row([numIt, xK, xKPlus1, abs(xKPlus1 - xK)])              # Gera as linhas da tabela
+        
+        if abs(xKPlus1 - xK) <= stopC :                                 # Verifica o Critério de Parada
+            print(t)
+            print("\nRaiz da função:", xKPlus1)
+            print("\n")
+            break
+        
+        xK = xKPlus1                                                    # Atualiza o valor de xK para próxima iteração
+        
+        numIt += 1                                                   
+        
+    if numIt == 200 :                                                   # Verifica a validade da Função de Iteração
+        print("\nFunção de Iteração inválida\n")
+        main()
+        
 
 def main():
 
     print("\nMétodo do Ponto Fixo")
-
-    print("\nDigite a função de iteração correspondente a função em questão:")
-    print("\nEx. f(x) = cos(x)-x -> g(x) = cos(x)")
-    str_g = input("\ng(x) = ")
-
-    g = lambda x : eval(str_g)                                          # Cria uma função para realizar a substituição de variável na equação
     
-    xk = float(input("\nDigite um valor inicial, xk, para realização da aproximação:"))
+    print("\nDigite a função:")
+    print("\nEx. f(x) = cos(x)-x")
+    str_f = input("\nf(x) = ")
+    
+    f = lambda x : eval(str_f) 
+    
+    print("\nDigite a Função de Iteração correspondente:")
+    print("\nEx. f(x) = cos(x)-x → F(x) = cos(x)")
+    str_F = input("\nF(x) = ")
+
+    F = lambda x : eval(str_F)                                          # Cria uma função para realizar a substituição de variável na equação
+    
+    print("\nEscolha um intervalo [α, β] tal que f(α) . f(β) < 0 :")
+    a = float(input("\nα = "))
+    b = float(input("\nβ = "))
+
+    while (f(a)*f(b) > 0) :                                             # Verifica se as entradas α e β satisfazem a condição
+        print("\nErro: f(",a,") =", f(a), ", f(",b,") =",f(b))
+        a = float(input("\nα = "))
+        b = float(input("\nβ = "))
+
+    print("\nEntradas aceitas: f(",a,") =", f(a),", f(",b,") =",f(b))
+    
+    if a > b:                                                           # Garante que o intervalo seja válido: β > α
+        aux = b
+        b = a
+        a = aux
+
+    print("\nIntervalo definido: [",a,",",b,"]")
+    
+    xK = float(input("\nDigite um valor inicial, x0, que esteja dentro do intervalo para realização da aproximação: "))
     
     stopC = float(eval(input("\nDigite o Critério de Parada desejado (Ex. 10**-3) : ")))
-    
-    maxIt = int(input("\nDigite o número máximo de iterações que o método deve realizar (Ex. 20) : "))
 
     print("\n")
     
-    fixedPointMethod(g, xk, stopC, maxIt)                             # Executa o Método do Ponto Fixo
+    fixedPointMethod(F, xK, stopC)                                      # Executa o Método do Ponto Fixo
 
 
 if __name__ == "__main__":
